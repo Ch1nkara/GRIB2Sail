4ee1a941f10929702621c548e045a2ae99f61ed0
chinkara <poubelledechinkara@outlook.com>
2026-02-03 23:34:28 -0400

    Feat: add GFS model resolution 0025

--- grib2sail/variables.py
+++ grib2sail/variables.py
@@ -1,3 +1,3 @@
-MODELS = ['arome_antilles', 'arome', 'arome0025', 'arome_guyane', 'arome_indien', 'arome_ncaledonie', 'arome_polynesie']
+MODELS = ['arome_antilles', 'arome', 'arome0025', 'arome_guyane', 'arome_indien', 'arome_ncaledonie', 'arome_polynesie', 'gfs']
 STEPS = ['1h', '3h', '6h', '12h']
 DATAS = ['wind', 'wind_gust', 'pressure', 'cloud', 'rain']
